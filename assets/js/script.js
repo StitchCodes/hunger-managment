bd5863787f7cc85f1058f8361f2da8e0c54618b7
StitchCodes <coderstitch@gmail.com>
2021-12-14 19:43:08 -0600

    Added function to search for input ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,18 +32,48 @@ function spoonCall() {
     .then(data => console.log(data));
 };
 
-// FUNCTION THAT CALLS SPOONACULAR API
-// function cocktailCall() {
+// FUNCTION THAT CALLS COCKTAILDB API
+function cocktailCall(ingredient) {
 //     // KEY
 //     let cocktailKey;
 
-//     // SPOONACULAR API
-//     fetch("https://api.spoonacular.com/recipes/716429/information?apiKey=" + spoonKey, {
-//         "method": "GET",
-//         "headers": {
-//         "Accept": 'application/json'
-//         }
-//     })
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-// };
+//     // COCKATAIL API
+    console.log(ingredient);
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient, {
+        "method": "GET",
+        "headers": {
+            'Accept': 'application/json'
+        }
+        })
+    .then(response => response.json())
+    .then(data => { 
+        // CALLS ALL DRINK NAMES
+        for (var i= 0; i < data.drinks.length; i++) {
+            console.log(data.drinks[i].strDrink)}
+        });
+};
+
+// FUNCTION TO SEARCH BY INGREDIENT SPOONACULAR
+function searchByIngredient() { 
+    let spoonKey = "f4a4ed51a308475a8fb85619133c516e";
+    let inputIngredients = ["bannana", "flour", "sugar"];
+
+    // FETCH API
+    fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + inputIngredients +"&number=10&apiKey=" + spoonKey, {
+        "method": "GET",
+        "headers": {
+        "Accept": 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => { 
+        // CALLS ALL DISH NAMES
+        for (var i= 0; i < data.length; i++) {
+            console.log(data[i].title)}
+        });
+};
+
+// COCKTAILDB CALL
+cocktailCall("tequila");
+searchByIngredient();
+// END OF JS
